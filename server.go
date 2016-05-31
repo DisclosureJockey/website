7c477cc951263dd94e59c71bc4612a9218813ad4
Brady Sullivan <brady@bsull.com>
2016-05-30 21:15:25 -0700

    Add an includeHandler to handle images, css, etc. Also allows for prototyping the pages.

--- server.go
+++ server.go
@@ -15,10 +15,21 @@ func main() {
 	fmt.Printf("Version v%s\n", version)
 
 	r := mux.NewRouter()
+	r.HandleFunc("/pages", includeHandler)
+	r.HandleFunc("/css", includeHandler)
+	r.HandleFunc("/js", includeHandler)
 	r.HandleFunc("/", HomeHandler)
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// Only use for files you want directly served.
+func includeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[+] Include handler called.")
+	filename := r.URL.Path[1:]
+	log.Println("Probably GET", filename, r)
+	http.ServeFile(w, r, filename)
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
